9f9ce219a4f106302a1754f178505aef61785978
Santosh Kandalkar <skandalkar1508@gmail.com>
2024-07-28 22:57:19 +0530

    *temp modified.

---
+++ script/script.js
@@ -0,0 +1,19 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.opacity = i === index ? 1 : 0;
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); // Change slide every 3 seconds
+
+// Initial call to show the first slide
+showSlide(currentSlide);
